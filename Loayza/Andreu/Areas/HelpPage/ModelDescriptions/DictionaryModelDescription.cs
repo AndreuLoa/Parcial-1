296979533c876bd7001ae875f34ba41b15bf2bf5
Andreu Loayza <andreu.loayza@hotmail.com>
2019-09-28 11:55:57 -0400

    commit inicial

---
+++ Loayza/Andreu/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Andreu.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Loayza/Andreu/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Andreu.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
