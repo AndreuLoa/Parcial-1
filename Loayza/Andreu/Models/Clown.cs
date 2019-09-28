d9aa3d1f4686c4b13a3c9e8af9fb2442436e8f64
Andreu Loayza <andreu.loayza@hotmail.com>
2019-09-28 12:08:08 -0400

    Commit modelos done

---
+++ Loayza/Andreu/Models/Clown.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Andreu.Models
+{
+    public class Clown
+    {
+        public enum lista
+        {
+            Alabar,
+            Andar,
+            Avion
+        }
+        [Key]
+        public int ClownID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(24, MinimumLength =2)]
+        public string NickName { get; set; }
+
+        [Required]
+        public lista Tricks { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Cumpleanos")]
+        public DateTime Birthdate { get; set; }
+    }
+}
