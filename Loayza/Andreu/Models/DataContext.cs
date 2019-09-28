7f9ee81f07ba70c1dc1fdff9d1e9b9f1a0b73556
Andreu Loayza <andreu.loayza@hotmail.com>
2019-09-28 12:15:51 -0400

    commit completo

---
+++ Loayza/Andreu/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Andreu.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Andreu.Models.Clown> Clowns { get; set; }
+    }
+}
